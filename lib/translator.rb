6a304178a9d009b8ed0b14049e571c8dcc503c80
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-05 06:03:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,11 +5,11 @@ def load_library (string)
   # code goes here
   emotemp = YAML.load_file(string)
   emot = {}
-  meaning = {}
-  emoticon = {}
+  meaning = []
+  emoticon = []
   emotemp.each do |key, emo|
-    meaning[emo[1]] = emo[0]
-    emoticon[emo[0]] = emo[1]
+    meaning << emo[0]
+    emoticon << emo[1]
   end
   emot[:get_meaning] = meaning
   emot[:get_emoticon] = emoticon
