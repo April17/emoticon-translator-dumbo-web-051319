1b3f9b809713d64b30c1be496e51b77531309ec6
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-05 06:18:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,8 +8,8 @@ def load_library (string)
   meaning = {}
   emoticon = {}
   emotemp.each do |key, emo|
-    meaning[emo[1]] = {}
-    emoticon[emo[0]] = {}
+    meaning[emo[1]] = key
+    emoticon[emo[0]] = emo[1]
   end
   emot["get_meaning"] = meaning
   emot["get_emoticon"] = emoticon
