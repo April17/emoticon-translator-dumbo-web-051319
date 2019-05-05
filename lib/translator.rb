9c7b41bee8c4a447826183c381d570e0789cd09b
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-05 05:17:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,11 @@
 # require modules here
-
-def load_library
+require "yaml"
+require "pry"
+def load_library (string)
   # code goes here
+  emo = YAML.load_file(string)
+  binding.pry
+  return emo
 end
 
 def get_japanese_emoticon
