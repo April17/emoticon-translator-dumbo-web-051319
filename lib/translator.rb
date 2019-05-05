e87a9830bc877a96ab98e0f343795e923ea16e67
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-05 05:44:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,12 @@ require "pry"
 def load_library (string)
   # code goes here
   emotemp = YAML.load_file(string)
-
+  emo = {}
+  emotemp.each do |key, emo|
+    emo[:get_meaning[emo[1]]] = emo[0]
+    emo[:get_emoticon[emo[0]]] = emo[1]
+  end
+  binding.pry
   return emotemp
 end
 
